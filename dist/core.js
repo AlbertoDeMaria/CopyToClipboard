c3853d2f3f25360705c9b34b442727fbb525375a
ADMStudio <demariaalbertowork@gmail.com>
2022-10-11 02:11:15 +0200

    edit

--- dist/core.js
+++ dist/core.js
@@ -3,11 +3,16 @@ const icon = document.querySelectorAll('#icon');
 const box = document.querySelectorAll('#box');
 
 box.forEach( (b, index) => {
-    b.addEventListener('click', () => {        
+    b.addEventListener('click', (e) => {
+        // copy target
+        copyTargetText(b);
+
+        // change icon and text on click        
         msg[index].innerHTML = 'Copied to clipboard';
         icon[index].classList.remove('fa-regular','fa-clone');
         icon[index].classList.add('fa-solid','fa-check');
-        
+                
+        // after 2.5s restore text and icon
         setTimeout(() => {
             msg[index].innerHTML = 'Copy to clipboard';
             icon[index].classList.add('fa-regular','fa-clone');
@@ -16,3 +21,18 @@ box.forEach( (b, index) => {
         
     })
 })
+
+async function copyTargetText(b) {
+    let text = '';
+    // remove white space and new lines
+    b.querySelectorAll('.row').forEach((r)=>{
+        text += r.innerText.replace(/(\r\n|\n|\r)/gm, "") + '\n';
+    });
+
+    // copy to Clipboard
+    try {
+      await navigator.clipboard.writeText(text);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  }
